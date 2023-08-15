660761833e4f0fa54ad35613afd407c8ce1531f4
gino <gamexgaming1997@gmail.com>
2023-08-16 04:00:49 +0800

    50% mobile responsive not yet finish continue tommorow

---
+++ app.js
@@ -0,0 +1,54 @@
+//get dom elements 
+const MobileNavElement = document.getElementById('mobile-dom-element');
+const HamburgerToggle = document.getElementById('toggle-hamburger'); // hamburger btn
+const ArrowDownCategoryToggle = document.getElementById('toggle-category'); // category btn
+const showElementMenu = document.getElementById('show-element-toggle'); // dom element for mobile nav menu
+const showCategoryMenu = document.getElementById('show-category-toggle'); // dom element for mobile category
+
+// boolean
+let ifHambugerToggle = false;
+let ifCategoryToggle = false;
+
+
+//  get mobile breakpoint
+const getMobileMedia = window.matchMedia("(min-width: 320px)");
+
+if (getMobileMedia.matches) {
+    MobileNavElement.classList.remove("mobile-dom-element");
+} else {
+    MobileNavElement.classList.add("mobile-dom-element");
+}
+
+HamburgerToggle.addEventListener('click', () => {
+
+    ifHambugerToggle = !ifHambugerToggle
+
+    if (getMobileMedia.matches && ifHambugerToggle) {
+        showElementMenu.classList.remove("showElementMenu-none");
+        showElementMenu.classList.add("showElementMenu")
+    } else {
+        showElementMenu.classList.remove("showElementMenu");
+        showElementMenu.classList.add("showElementMenu-none")
+    }
+})
+
+ArrowDownCategoryToggle.addEventListener('click', () => {
+
+    ifCategoryToggle = !ifCategoryToggle
+
+    if (getMobileMedia.matches && ifCategoryToggle) {
+        showCategoryMenu.classList.remove("showCategoryElement-none");
+        showCategoryMenu.classList.add("showCategoryElement")
+    } else {
+        showCategoryMenu.classList.remove("showCategoryElement");
+        showCategoryMenu.classList.add("showCategoryElement-none")
+    }
+})
+
+
+
+
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+tailwind.config = {
+    theme: {
+        fontSize: {
+            sm: '0.8rem',
+            md: '0.9rem',
+            base: '1rem',
+            xl: '1.15rem',
+            '1xl': '1.35em',
+            '2xl': '1.65em',
+            '3xl': '1.953rem',
+            '4xl': '2.441rem',
+            '5xl': '3.052rem',
+        },
+        extend: {
+            fontFamily: {
+                Oswald: ["Oswald", "sans-serif"],
+                Oxygen: ["Oxygen", "sans-serif"],
+                Poppins: ["Poppins", "sans-serif"],
+                Gothic: ["Gothic", "sans-serif"]
+            },
+            // backgroundImage: theme => ({
+            //     'bg-nft': "url('../assets/projectsimg/nft.jpg')",
+            //     'bg-portfolio': "url('../assets/projectsimg/portfolio.jpg')",
+            //     'bg-animefreak': "url('../assets/projectsimg/animefreak.jpg')",
+            // })
+        },
+        screens: {
+            xs: "320px", // s-phone
+            sss: "405px", // m-phone
+            ss: "620px", // s-tablet
+            sm: "768px", // tablet
+            md: "1024px", // tablet - ipad
+            lg: "1280px", // ipad
+            lg1: "1366px", // laptop
+            lg2: "1660px", // computer
+            xl: "1920px", // computer-l
+        },
+    },
+    plugins: [],
+};
